090595f1a000f19f1e29b1a052bc248be6561c6d
alec quig <alecoralec@gmail.com>
2020-02-21 17:27:58 -0600

    at 'useage in background-image' part of lecture source, have to go to quiz review

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy # if you destroy a cocktail, the doses are also destroyed
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  has_one_attached :photo
 end
